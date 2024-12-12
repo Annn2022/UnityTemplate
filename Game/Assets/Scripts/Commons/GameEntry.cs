5676d66b7dc351e66eef5eee1e21e94310706053
shuan <ee>
2024-12-12 14:27:06 +0800

    init

---
+++ Game/Assets/Scripts/Commons/GameEntry.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel;
+using UnityEngine;
+
+namespace Commons
+{
+    [Description("游戏主入口，负责初始化游戏进程")]
+    public class GameEntry : MonoBehaviour
+    {
+        void Start()
+        {
+        
+        }
+    
+    }
+}
